92c37635cc88f0dfb1921b2288f181d2eafeab23
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2022-01-06 00:35:41 +0300

    add

--- fua-app/src/routes/index.svelte
+++ fua-app/src/routes/index.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let title = 'my homepage';
+    const updateTitle = () => {
+        title  = "welcom"
+    }
+</script>
+
+<div class="index">
+    <h2>{title}</h2>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit nam, minus tenetur ab explicabo voluptate quis at tempore cupiditate ex sint quae nihil consectetur. Esse culpa nesciunt accusamus aliquam. Doloremque?</p>
+
+    <button on:click={updateTitle}>Change title</button>
+    <input type="text" bind:value={title}>
+</div>
+
+<style>
+    .index{
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
