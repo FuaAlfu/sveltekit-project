e2a0ddf3a8909dd1cba3e836f1b4011e41144510
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2022-01-10 01:47:48 +0300

    add new

---
+++ fua-app/src/routes/guides/test.svelte
@@ -0,0 +1,14 @@
+<div class="test">
+    <h1>testing</h1>
+
+    <a href="/about">About</a>
+    <a href="/guides">View Guides</a>
+</div>
+
+<style>
+     .test{
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+     }
+</style>
--- fua-app/src/routes/index.svelte
+++ fua-app/src/routes/index.svelte
@@ -10,7 +10,10 @@
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit nam, minus tenetur ab explicabo voluptate quis at tempore cupiditate ex sint quae nihil consectetur. Esse culpa nesciunt accusamus aliquam. Doloremque?</p>
 
     <button on:click={updateTitle}>Change title</button>
-    <input type="text" bind:value={title}>
+    <input type="text" bind:value={title}><br>
+    <a href="/about">About</a>
+    <a href="/guides">View Guides</a>
+    <a href="/test">Click Me</a>
 </div>
 
 <style>
