ab50e86ad43b4dc5d4200f6f5810069cd5c908f9
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2022-01-10 01:37:29 +0300

    add

---
+++ fua-app/src/routes/about.svelte
@@ -0,0 +1,14 @@
+<div class="about">
+    <h2>about us</h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit distinctio eos sequi maxime repellendus quam maiores sapiente, aut impedit eius nesciunt. Dignissimos a corrupti necessitatibus! Atque quidem provident quaerat dolor!</p><br>
+    <a href="/about">About</a>
+    <a href="/guides">View Guides</a>
+</div>
+
+<style>
+    .about{
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
---
+++ fua-app/src/routes/guides/hello.svelte
@@ -0,0 +1,3 @@
+<div>
+    <h1>hello everyone !</h1>
+</div>
