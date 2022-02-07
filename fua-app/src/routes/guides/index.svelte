b8b36cd91472893830b93ceabe60b279b80ecb75
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2022-02-07 11:45:26 +0300

    add

--- fua-app/src/routes/guides/index.svelte
+++ fua-app/src/routes/guides/index.svelte
@@ -1,7 +1,37 @@
+<!-- context is special attribute, distinguish between two scripts tag -->
+<script context="module">
+    export async function load({fetch}){  //export async function load(context)
+        //context.fetch();
+       const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+       const guides = await res.json()
+
+       if(res.ok){
+           return{
+               props:{
+                   guides: guides
+               }
+           }
+       }
+       return{
+           status: res.status,
+           error: new Error("Could not fetch the guides")
+       }
+    }
+</script>
+
+<!-- main script tag -->
+<script>
+    export let guides
+</script>
 <div class="guides">
     <ul>
-        <li><a href="/">guide 1</a></li>
-        <li><a href="/">guide 1</a></li>
+        <!-- <li><a href="/">guide 1</a></li>
+        <li><a href="/">guide 1</a></li> -->
+        {#each guides as g}
+            <li>
+                <a herf="/guides">{g.title}</a>
+            </li>
+        {/each}
     </ul>
 </div>
 
